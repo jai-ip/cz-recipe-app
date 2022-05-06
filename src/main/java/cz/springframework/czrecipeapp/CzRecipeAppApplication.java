bc114fef7313e85e37c2cc860726258861956a16
jaihunter <jayachandranjaicz@gmail.com>
2022-05-06 12:02:35 +0530

    Initilize project

---
+++ src/main/java/cz/springframework/czrecipeapp/CzRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package cz.springframework.czrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzRecipeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/springframework/czrecipeapp/CzRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.springframework.czrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
