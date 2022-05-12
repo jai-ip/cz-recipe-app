b71b0a7d65351f27871b561b94998327d9b86fdc
jaihunter <jayachandranjaicz@gmail.com>
2022-05-12 13:50:42 +0530

    Added relationship btw recipe and category.

---
+++ src/main/java/cz/springframework/czrecipeapp/model/Category.java
@@ -0,0 +1,44 @@
+package cz.springframework.czrecipeapp.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+                joinColumns = @JoinColumn(name = "recipe_id"),
+                inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
